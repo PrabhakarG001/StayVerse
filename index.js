bb1316ab27e2b9c724cdbb719eb8ed603f72dac6
Prabhakar Gupta <Prabhakarg465@gmail.com>
2026-07-14 23:17:16 +0530

    Fix Render deployment by moving index.js to repo root

---
+++ index.js
@@ -0,0 +1,2 @@
+// This file exists to satisfy Render's default start command (node index.js)
+require('./app.js');
--- src/index.js
+++
@@ -1,2 +0,0 @@
-// This file exists to satisfy Render's default start command (node src/index.js)
-require('../app.js');
